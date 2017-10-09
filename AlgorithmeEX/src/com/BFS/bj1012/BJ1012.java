bca16115c46a3c34d8d61847264a7c4078a8298c
SeungChan Baek <scfreedom31@gmail.com>
2017-10-09 20:53:51 +0900

    백준 1012 BFS, DFS

---
+++ AlgorithmeEX/src/com/BFS/bj1012/BJ1012.java
@@ -0,0 +1,89 @@
+package com.BFS.bj1012;
+import java.util.*;
+
+public class BJ1012 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int testCase = in.nextInt();
+		int [][][] adj = new int [testCase][][];
+		boolean [][][] visited = new boolean [testCase][][];
+		int [] result = new int [testCase];
+		for(int i=0; i<testCase; i++)
+		{
+			int m = in.nextInt();
+			int n = in.nextInt();
+			int k = in.nextInt();
+			adj[i] = new int [n][m]; 
+			visited[i] = new boolean [n][m];
+			for(int j=0; j<k; j++)
+			{
+				int trueY = in.nextInt();
+				int trueX = in.nextInt();
+				adj[i][trueX][trueY] = 1;
+			}
+		}
+		
+		bfs(adj, visited, result);
+		
+
+	}
+	
+	static void bfs(int [][][] adjMatrix, boolean [][][] visitMatrix, int[] resultCount) {
+		
+		int[] moveX = {1,-1,0,0};
+		int[] moveY = {0,0,1,-1};
+		Queue<Point> queue = new LinkedList<>();
+		
+		for(int firstCount=0; firstCount < adjMatrix.length; firstCount++)
+		{
+			for(int secondCount=0; secondCount < adjMatrix[firstCount].length; secondCount++)
+			{
+				for(int thirdCount=0; thirdCount < adjMatrix[firstCount][secondCount].length; thirdCount++)
+				{
+					if(adjMatrix[firstCount][secondCount][thirdCount] == 1 && visitMatrix[firstCount][secondCount][thirdCount]==false)
+					{
+						queue.offer(new Point(secondCount,thirdCount));
+						while(!queue.isEmpty())
+						{
+							Point current = queue.poll();
+							int currentX = current.x;
+							int currentY = current.y;
+							for(int i=0; i<4; i++)
+							{
+								int nextX = current.x + moveX[i];
+								int nextY = current.y + moveY[i];
+								
+								if(nextX < 0 || nextY < 0 || nextX >= adjMatrix[firstCount].length || nextY >= adjMatrix[firstCount][secondCount].length)
+									continue;
+								if(adjMatrix[firstCount][nextX][nextY] == 1 && visitMatrix[firstCount][nextX][nextY]==false)
+								{
+									visitMatrix[firstCount][nextX][nextY]=true;
+									queue.offer(new Point(nextX,nextY));
+								}								
+							}
+						}
+						
+						resultCount[firstCount]++;
+						
+					}
+					
+				}
+			}
+		}
+		
+		for(int i=0; i<resultCount.length; i++)
+			System.out.println(resultCount[i]);
+	}
+	
+	
+    static class Point{
+    	int x, y;
+		public Point (int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+}
---
+++ AlgorithmeEX/src/com/DFS/bj1012/BJ1012.java
@@ -0,0 +1,78 @@
+package com.DFS.bj1012;
+import java.util.*;
+
+public class BJ1012 {
+	static Scanner in = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+
+		int testCase = in.nextInt();
+		int[] resultCount = new int [testCase];
+		
+		while(testCase > 0)
+		{
+			int m = in.nextInt();
+			int n = in.nextInt();
+			int k = in.nextInt();
+			
+			int[][] adj = new int[n][m];
+			makeGraph(adj, k);
+			boolean[][] visited = new boolean[n][m];
+			
+			for(int i=0; i<adj.length; i++)
+			{
+				for(int j=0; j<adj[i].length; j++)
+				{
+					if(adj[i][j] == 1 && visited[i][j] == false)
+					{
+						visited[i][j] = true;
+						dfs(adj, visited, i,j);
+						resultCount[testCase-1]++;
+					}
+						
+				}
+			}
+			
+			testCase--;
+		}
+		
+		for(int i = resultCount.length; i>0; i--)
+			System.out.println(resultCount[i-1]);
+		
+	}
+	
+	public static void dfs(int[][] adjMatrix, boolean[][] visitMatrix, int pointX, int pointY) {
+		
+		int currentX = pointX;
+		int currentY = pointY;
+		
+		
+		int[] moveX = {1, -1, 0, 0};
+		int[] moveY = {0, 0, 1, -1};
+		
+		for(int i=0; i<4; i++)
+		{
+			int nextX = currentX + moveX[i];
+			int nextY = currentY + moveY[i];
+			
+			if(nextX < 0 || nextY < 0 || nextX >= adjMatrix.length || nextY >= adjMatrix[currentX].length)
+				continue;
+			if(adjMatrix[nextX][nextY] == 1 && visitMatrix[nextX][nextY] == false)
+			{
+				visitMatrix[nextX][nextY] = true;
+				dfs(adjMatrix, visitMatrix, nextX, nextY);
+			}
+		}
+		
+	}
+	
+	public static void makeGraph(int[][] adjMatrix, int caseSize) {
+		for(int i=0; i<caseSize; i++)
+		{
+			int pointY = in.nextInt();
+			int pointX = in.nextInt();
+			adjMatrix[pointX][pointY] = 1;
+		}
+	}
+	
+}
