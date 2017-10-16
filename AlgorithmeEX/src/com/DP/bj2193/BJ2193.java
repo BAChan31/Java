352ce3a6138fada0c829a7a9077ae7e9c2ccbfdd
SeungChan Baek <scfreedom31@gmail.com>
2017-10-16 11:34:02 +0900

    수정....

--- AlgorithmeEX/src/com/DP/bj2193/BJ2193.java
+++ AlgorithmeEX/src/com/DP/bj2193/BJ2193.java
@@ -7,52 +7,18 @@ public class BJ2193 {
 
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		int[][] arr = new int[n+1][];
-		int[] dp = new int[n+1];
-		int jegob = 1;
+		long[] dp = new long [n+1];
 		
-		for(int i=1; i<arr.length; i++)
-		{
-			if(i<2^jegob)
-			{
-				
-			}
-			int count = i/(2^jegob);
-			arr[i] = new int[count+1];
-			int divideResult = i;
-			for(int j=arr[i].length-1; j>=0; j--)
-			{
-
-				int divideLeft = divideResult%2;
-				arr[i][j] = divideLeft;
-				divideResult = divideResult/2;				
-			}
-		}
+		dp[0] = 0;
+		dp[1] = 1;
 		
-		for(int i=1; i<arr.length; i++)
+		for(int i=2; i<=n; i++)
 		{
-			for(int j=0; j<arr[i].length; j++)
-			{
-				if(j==0 && i==1)
-				{
-					dp[i]++;
-					break;
-				}
-				else if(j==0 && i!=1)
-					continue;
-				if(!(arr[i][j]==1 && arr[i][j-1] == 1))
-				{
-					dp[i] = dp[i-1] +1;
-					break;
-				}
-				else
-				{
-					dp[i] = dp[i-1];
-				}
-			}
+			dp[i] = dp[i-1] + dp[i-2];
 		}
 		
 		System.out.println(dp[n]);
+		
 	}
 
 }
