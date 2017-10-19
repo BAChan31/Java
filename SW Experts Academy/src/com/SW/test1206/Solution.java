ee24cc574d4babfdd30866df32889845f20cb853
SeungChan Baek <scfreedom31@gmail.com>
2017-10-19 12:59:48 +0900

    풀긴 했는데 찜찜하다

---
+++ SW Experts Academy/src/com/SW/test1206/Solution.java
@@ -0,0 +1,37 @@
+package com.SW.test1206;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int testCase=1;
+		
+		while(testCase <= 10)
+		{
+			int totalCount = 0;
+			int widthSize = in.nextInt();
+			int[] apartment = new int[widthSize];
+
+			for(int i=0; i<widthSize; i++)
+			{
+				apartment[i] = in.nextInt();
+			}
+			
+			for(int i=2; i<widthSize-2; i++)
+			{
+				int leftSide = Math.max(apartment[i-2], apartment[i-1]);
+				int rightSide = Math.max(apartment[i+1], apartment[i+2]);
+				int maxTop = Math.max(leftSide, rightSide);
+				
+				if(apartment[i] > maxTop)
+				{
+					totalCount += (apartment[i] - maxTop);
+				}
+			}
+			
+			System.out.println("#" + testCase + " " + totalCount );
+			testCase++;
+		}
+	}
+}
--- SW Experts Academy/src/com/SW/test2105/TEST2105.java
+++ SW Experts Academy/src/com/SW/test2105/TEST2105.java
@@ -5,22 +5,22 @@ public class TEST2105 {
 	
 	public static final int[] moveX = {-1,-1,1,1};
 	public static final int[] moveY = {-1, 1,1,-1};
-	public static final int STARTPOINT = 2;
-	public static final int CANVISIT = 1;
-	public static final int CANNOTVISIT = 0;
+
+	public static int testResult;
+	public static int [][]graph;
+	public static boolean[] dessertCafe;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		int testCase = in.nextInt();
-		int[][] graph;
-		int[] resultArr = new int[testCase];
-		
-		while(testCase>0)
+
+		for(int testCaseNum=1; testCaseNum <= testCase; testCaseNum++)
 		{
 			int caseNum = in.nextInt();
 			graph = new int[caseNum][caseNum];
 			
+			testResult = -1;
 			for(int i=0; i<caseNum; i++)
 			{
 				for(int j=0; j<caseNum; j++)
@@ -29,16 +29,13 @@ public class TEST2105 {
 				}
 			}
 			
-			foundBest(graph, caseNum);
-			
-			testCase--;
+			foundBest(caseNum);
+			System.out.println("#" + testCaseNum + " " + testResult);
 			
 		}
 	}
 	
-	static void foundBest(int[][] graph, int caseCount) {
-		int temp = 0;
-		int [][] visitGraph;
+	static void foundBest(int caseCount) {
 
 		
 		//그래프별로 시작점 설정
@@ -46,68 +43,51 @@ public class TEST2105 {
 		{
 			for(int j=0; j<caseCount; j++)
 			{
-				boolean[] dessert = new boolean[101];
-				visitGraph = new int[caseCount][caseCount];
-				Point startPoint = new Point(i,j);
-				findWay(graph, visitGraph, dessert, startPoint, i, j, true);
+				dessertCafe = new boolean[101];
+				findWay(i,j,i,j,0, 0, 0,true);
 			}
 		}
 		
-		System.out.println("dfdf");
 	}
 	
-	static void findWay(int[][] adj, int[][] checkedGraph, boolean[] visitCafe ,Point current, int direction, int curveCount, boolean start) {
-		if(start)
-		{
-			checkedGraph[current.x][current.y] = STARTPOINT;
-			visitCafe[adj[current.x][current.y]] = true;
-		}
+	static void findWay(int startX, int startY, int currentX, int currentY, int direction, int curveCount, int dessertCount, boolean start) {
+
+		if(curveCount > 3)
+			return;
 		
 		for(int i=0; i<4; i++)
 		{
-			Point nextPoint = new Point(current.x,current.y);
-			nextPoint.x += moveX[i];
-			nextPoint.y += moveY[i];
-			if(start)
-			{
-				direction = i;
-				curveCount = 0;
-			}
-			else if(direction != i)
-			{
-				direction = i;
-				curveCount ++;
-			}
-			
-			//for문 탈출조건
-			if(nextPoint.x < 0 || nextPoint.y < 0 || nextPoint.x >= adj.length || nextPoint.y >= adj[current.x].length)
+			int nextX = currentX + moveX[i];
+			int nextY = currentY + moveY[i];
+					
+			//조건을 만족시키지 못했을 때
+			if(nextX < 0 || nextY < 0 || nextX >= graph.length || nextY >= graph[currentX].length)
 				continue;
-			if(curveCount > 3)
-				return;
-			if(visitCafe[adj[nextPoint.x][nextPoint.y]] == true)
+
+			if(dessertCafe[graph[nextX][nextY]])
 				continue;
-			if(checkedGraph[nextPoint.x][nextPoint.y]==STARTPOINT && curveCount == 3)
-				return;
-			if(checkedGraph[nextPoint.x][nextPoint.y] == CANNOTVISIT && curveCount <= 3)
+			//모든 조건을 만족하였을 때
+			if((nextX==startX && nextY==startY) && curveCount >= 2)
 			{
-				visitCafe[adj[nextPoint.x][nextPoint.y]] = true;
-				checkedGraph[nextPoint.x][nextPoint.y] = CANVISIT;
-				findWay(adj, checkedGraph, visitCafe, nextPoint, direction, curveCount, false);
-				visitCafe[adj[nextPoint.x][nextPoint.y]] = true;
-				checkedGraph[nextPoint.x][nextPoint.y] = CANNOTVISIT;
+				testResult = Math.max(testResult, dessertCount +1);
+				return;
 			}
+			//다음 조건으로 넘어가기 전 설정
 			
-			
+				int nextCurve=curveCount;
+				
+				if(!start)
+				{
+					if(direction != i)
+					{
+						nextCurve=curveCount+1;
+					}
+				}
+				
+				dessertCafe[graph[nextX][nextY]] = true;
+				findWay(startX, startY, nextX, nextY, i, nextCurve, dessertCount+1,false);
+				dessertCafe[graph[nextX][nextY]] = false;			
 		}
 	}
 	
-
-	public static class Point {
-		int x,y;
-		public Point(int x, int y) {
-			// TODO Auto-generated constructor stub
-			this.x = x;
-			this.y = y;			
-		}
-	}
 }
