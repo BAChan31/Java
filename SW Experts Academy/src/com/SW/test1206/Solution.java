37e62b2291a15b593ccad92775b4c559c3627c2e
SeungChan Baek <scfreedom31@gmail.com>
2017-10-22 11:53:10 +0900

    수정

--- SW Experts Academy/src/com/SW/test1206/Solution.java
+++ SW Experts Academy/src/com/SW/test1206/Solution.java
@@ -29,7 +29,6 @@ public class Solution {
 					totalCount += (apartment[i] - maxTop);
 				}
 			}
-			
 			System.out.println("#" + testCase + " " + totalCount );
 			testCase++;
 		}
---
+++ SW Experts Academy/src/com/SW/test1953/Solution.java
@@ -0,0 +1,47 @@
+package com.SW.test1953;
+import java.util.*;
+
+public class Solution {
+
+	public static int[][][] MAP;
+	public static boolean[][] VISIT;
+	public static int[] moveX = {-1,0,1,0,-1};
+	public static int[] moveY = {0,1,0,-1,0};
+	public static int RESULT;
+	
+	public static int[][] turnelType = { {0,0,0,0},{1,1,1,1},{1,0,1,0},{0,1,0,1},{1,1,0,0},{0,1,1,0},{0,0,1,1},{1,0,0,1}};
+	
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int testCase = in.nextInt();
+		int height, width, pointR, pointC, timeL;
+		for(int caseCount=1; caseCount<=testCase; caseCount++)
+		{
+			RESULT = 0;
+			
+			height = in.nextInt();
+			width = in.nextInt();
+			pointR = in.nextInt();
+			pointC = in.nextInt();
+			timeL = in.nextInt();
+		
+			MAP = new int[height][width][4];
+			VISIT = new boolean[height][width];
+			for(int i=0; i<height; i++) 
+			{
+				for(int j=0; j<width; j++)
+				{
+					int type = in.nextInt();
+					MAP[i][j] = turnelType[type];
+				}
+			}
+
+			System.out.println("#" + caseCount + " " + RESULT);
+		}
+	}
+	
+	static void findWay(int startX, int startY, int time, int resultValue) {
+		
+	}
+	
+}
